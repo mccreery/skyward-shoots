dda00f097ea5e96109f818904c6059e51f14cc8a
Sam McCreery <4602020+mccreery@users.noreply.github.com>
2021-05-01 00:11:38 +0100

    Make flower head follow sun

--- Assets/Scripts/Flower.cs
+++ Assets/Scripts/Flower.cs
@@ -3,11 +3,15 @@ using UnityEngine;
 
 public class Flower : MonoBehaviour
 {
+    public Transform head;
     public GameObject petalPrefab;
     public int numPetals = 8;
 
     public float maxRadius = 2;
     public float minRadius = 1;
+
+    public float speed = 1.0f;
+    public Transform sun;
 
     private List<GameObject> petals = new List<GameObject>();
 
@@ -45,13 +49,19 @@ public class Flower : MonoBehaviour
         }
     }
 
+    private void Update()
+    {
+        transform.position += head.up * speed * Time.deltaTime;
+        head.rotation = Quaternion.Euler(0, 0, sun.rotation.eulerAngles.z - 90);
+    }
+
     private GameObject GeneratePetal(int i)
     {
         float degrees = i * 360.0f / numPetals;
         float radians = degrees * Mathf.Deg2Rad;
 
         Vector2 petalPosition = new Vector2(maxRadius * Mathf.Cos(radians), minRadius * Mathf.Sin(radians));
-        GameObject petal = Instantiate(petalPrefab, petalPosition, Quaternion.Euler(0, 0, degrees), transform);
+        GameObject petal = Instantiate(petalPrefab, petalPosition, Quaternion.Euler(0, 0, degrees), head);
 
         return petal;
     }
