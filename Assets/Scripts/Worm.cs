3517b4325cafc302f4a171dbe2c2ceeb4bd45e52
Sam McCreery <4602020+mccreery@users.noreply.github.com>
2021-05-01 17:33:17 +0100

    Add worm collider

--- Assets/Scripts/Worm.cs
+++ Assets/Scripts/Worm.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
@@ -9,14 +10,27 @@ public class Worm : MonoBehaviour
     public float rotateRandomSpeed;
     public float speed;
 
+    public float colliderDistance = 0.1f;
+    public int colliderCount = 10;
+
+    public GameObject colliderPrefab;
+
     private float targetRotation;
 
     private float rotation;
     private float rotationVelocity;
 
+    private List<GameObject> colliders;
+
     private void Start()
     {
         targetRotation = Random.Range(-180, 180);
+
+        colliders = new List<GameObject>();
+        for (int i = 0; i < colliderCount; i++)
+        {
+            colliders.Add(Instantiate(colliderPrefab));
+        }
     }
 
     private void Update()
@@ -34,6 +48,16 @@ public class Worm : MonoBehaviour
         else
         {
             targetRotation += Random.Range(-rotateRandomSpeed, rotateRandomSpeed) * Time.deltaTime;
+        }
+
+        if ((colliders[0].transform.position - transform.position).sqrMagnitude > colliderDistance * colliderDistance)
+        {
+            GameObject tailCollider = colliders[colliders.Count - 1];
+            colliders.RemoveAt(colliders.Count - 1);
+
+            tailCollider.transform.position = transform.position;
+            tailCollider.transform.rotation = transform.rotation;
+            colliders.Insert(0, tailCollider);
         }
     }
 
