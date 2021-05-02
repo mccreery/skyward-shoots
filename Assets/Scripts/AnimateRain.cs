b264a471ccccc40c0312c744558c4a0c125f15f8
Sam McCreery <4602020+mccreery@users.noreply.github.com>
2021-05-02 13:30:03 +0100

    Add rain

---
+++ Assets/Scripts/AnimateRain.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class AnimateRain : MonoBehaviour
+{
+    public float angle;
+    public float speed;
+    public float fadeSpeed;
+
+    public Color color;
+
+    public bool showRain;
+
+    private Vector2 OffsetStep => new Vector2(Mathf.Cos(angle) * speed, Mathf.Sin(angle) * speed);
+
+    private Material material;
+
+    private void Awake()
+    {
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+        material = new Material(spriteRenderer.material);
+        spriteRenderer.material = material;
+        material.color = color;
+    }
+
+    private void Update()
+    {
+        material.mainTextureOffset += OffsetStep * Time.deltaTime;
+
+        float targetOpacity = showRain ? color.a : 0.0f;
+
+        Color fadedColor = material.color;
+        fadedColor.a = Mathf.MoveTowards(fadedColor.a, targetOpacity, fadeSpeed * Time.deltaTime);
+        material.color = fadedColor;
+    }
+}
