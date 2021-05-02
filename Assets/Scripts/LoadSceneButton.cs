5087875933203148d35441532c6730a64c0a42bd
jessm12 <jessmccreery@hotmail.co.uk>
2021-05-02 17:01:27 +0100

    menu sounds

--- Assets/Scripts/LoadSceneButton.cs
+++ Assets/Scripts/LoadSceneButton.cs
@@ -5,9 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class LoadSceneButton : MonoBehaviour
 {
+    public AudioClip select;
     public int sceneNumber;
     public void LoadScene()
     {
+        GameObject.Find("Menu Audio Source").GetComponent<AudioSource>().PlayOneShot(select);
         SceneManager.LoadScene(sceneNumber);
     }
 }
